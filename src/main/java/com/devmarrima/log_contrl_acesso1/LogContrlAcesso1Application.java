d34156a11ce56e5d7a18bb1a514e2a10ee9c682c
curso_básico_Dio <marrimacelso@gmail.com>
2025-03-29 12:30:00 -0300

    first commit

---
+++ src/main/java/com/devmarrima/log_contrl_acesso1/LogContrlAcesso1Application.java
@@ -0,0 +1,13 @@
+package com.devmarrima.log_contrl_acesso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogContrlAcesso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogContrlAcesso1Application.class, args);
+	}
+
+}
